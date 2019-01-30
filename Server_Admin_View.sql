ab0fdc390981bbba85a4530f79a8c454ca4f1bf5
Nicky Rucker <nickyrucker@hotmail.com>
2019-01-30 15:09:13 -0700

    Update Server_Admin_View.sql

--- Server_Admin_View.sql
+++ Server_Admin_View.sql
@@ -11,7 +11,7 @@ select distinct
   u.friendly_name USER_NAME,
   u.name USER_NAME_SHORT,
   uv.email EMAIL,
-  uv.login_at LAST_LOGIN_AT
+  uv.login_at::timestamp at time zone 'UTC' at time zone 'MST' as LAST_LOGIN_AT
 from
   permissions_templates pt
   join projects p
@@ -45,18 +45,18 @@ SELECT
 --   wb.view_count AS workbook_view_count,
   wb.owner_id AS workbook_owner_id,
   wb.owner_name AS workbook_owner_name,
-  wb.updated_at AS workbook_updated_at,
+  wb.updated_at::timestamp at time zone 'UTC' at time zone 'MST' AS workbook_updated_at,
   vs.views_id AS view_id,
   vs.views_name AS view_name,
   CAST(vs.views_title AS TEXT) AS view_title,
   CAST(vs.views_url AS TEXT) AS view_url,
   vs.users_id AS user_id,
   vs.system_users_friendly_name AS user_name,
-  vs.last_view_time AS user_last_viewed_date,
+  vs.last_view_time::timestamp at time zone 'UTC' at time zone 'MST' AS user_last_viewed_date,
 --   CAST(vs.device_type AS TEXT) AS view_device_type,
   vs.nviews AS user_total_views,
 --   vs.system_users_name AS user_name,
-  vs.users_login_at AS user_last_login  --,
+  vs.users_login_at::timestamp at time zone 'UTC' at time zone 'MST' AS user_last_login  --,
 --   vs.system_users_id AS view_system_users_id,
 --   vs.views_owner_id AS views_owner_id,
 --   vs.views_index AS views_index,
